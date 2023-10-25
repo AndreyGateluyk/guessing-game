e79aa5bc673012dec96284080d1cc3d41e4e9a45
Andrey Gateluyk <gatelyuk97@mail.ru>
2023-10-25 20:46:54 +0300

    complite

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
     }
 
+    setRange(min, max) {
+      this.min = min;
+      this.max = max;
+    }
+    
     guess() {
-
+      return this.expected = Math.floor(Math.random() * (this.max - this.min)) + this.min;
     }
 
     lower() {
-
+      this.max = Math.floor((this.min + this.max) / 2)
     }
 
     greater() {
-
+      this.min = Math.floor((this.min + this.max) / 2)
     }
 }
 
